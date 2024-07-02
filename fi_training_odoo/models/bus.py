8cafb93beb6c98061f18f7af4c5ae6a886a16fb7
moaibad <fathulibad.412@gmail.com>
2024-07-02 13:47:30 +0700

    complete all advanced field exercise

--- fi_training_odoo/models/bus.py
+++ fi_training_odoo/models/bus.py
@@ -4,8 +4,11 @@ from odoo import models, fields, api
 class Bus(models.Model):
     _name = 'res.bus'
     _description = 'Bus information'
+    _sql_constraints = [
+        ('code_unique', 'unique(code)', 'The code must be unique!')
+    ]
 
     name = fields.Char(string='Name')
-    code = fields.Char(string='Code')
+    code = fields.Char(string='Code', required=True)
     capacity = fields.Integer(string='Capacity')
     image = fields.Image(string='Image')
--- fi_training_odoo/models/bus_schedule.py
+++ fi_training_odoo/models/bus_schedule.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from datetime import datetime
+from odoo.exceptions import ValidationError, UserError
 
 
 class BusSchedule(models.Model):
@@ -6,7 +8,7 @@ class BusSchedule(models.Model):
     _description = 'Bus schedule information'
 
     name = fields.Char(string='Name')
-    schedule = fields.Datetime(string="Schedule")
+    date_of_issue = fields.Datetime(string="Date of Issue", default=lambda self: datetime.today(), readonly=True)
     payment_method = fields.Selection(
         [
             ("cash","Cash"),
@@ -18,3 +20,17 @@ class BusSchedule(models.Model):
     route_id = fields.Many2one(comodel_name='bus.route', string="Bus Route")
     baggage_ids = fields.One2many(comodel_name='baggage.baggage', inverse_name='schedule_id', string='Baggage')
     passenger_ids = fields.Many2many('res.passenger', string='Passenger')
+    capacity = fields.Integer(string="Capacity", related="bus_id.capacity",)
+
+    @api.constrains('arrival', 'departure')
+    def _check_arrival_time(self):
+        for record in self:
+            if record.arrival and record.arrival < record.departure:
+                raise UserError("Arrival time cannot be earlier than Departure time..")
+            
+    @api.constrains('passenger_ids', 'capacity')
+    def _check_capacity(self):
+        for record in self:
+            if record.capacity and len(record.passenger_ids) > record.capacity:
+                raise ValidationError("The bus is at maximum capacity..")
+    
