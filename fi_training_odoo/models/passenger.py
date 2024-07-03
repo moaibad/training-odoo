4ee0583617997e71ee5219c7d63e1aa80f75cb1c
moaibad <fathulibad.412@gmail.com>
2024-07-03 11:13:46 +0700

    add age field to passenger model

--- fi_training_odoo/models/passenger.py
+++ fi_training_odoo/models/passenger.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+from datetime import datetime
 
 class Passenger(models.Model):
     _name = 'res.passenger'
@@ -9,3 +9,15 @@ class Passenger(models.Model):
     weight = fields.Float(string='Weight(Kg)')
     height = fields.Float(string='Height(Cm)')
     birth_date = fields.Date(string='Birth Date')
+    age = fields.Integer(string='Age', compute='_compute_age', store=False)
+    
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for record in self:
+            if record.birth_date:
+                today = datetime.today()
+                birth_date = fields.Date.from_string(record.birth_date)
+                age = today.year - birth_date.year                
+                record.age = age
+            else:
+                record.age = 0
