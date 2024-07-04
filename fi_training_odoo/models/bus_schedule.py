3db81f8d3fb600334dfd4171837f4e68af6a9f34
moaibad <fathulibad.412@gmail.com>
2024-07-04 12:56:14 +0700

    add tracker to bus schedule model

--- fi_training_odoo/models/bus_schedule.py
+++ fi_training_odoo/models/bus_schedule.py
@@ -6,22 +6,23 @@ from odoo.exceptions import ValidationError, UserError
 class BusSchedule(models.Model):
     _name = 'bus.schedule'
     _description = 'Bus schedule information'
+    _inherit = ['mail.thread','mail.activity.mixin']
 
     name = fields.Char(string='Name')
-    date_of_issue = fields.Datetime(string="Date of Issue", default=lambda self: datetime.today(), readonly=True)
+    date_of_issue = fields.Datetime(string="Date of Issue", default=lambda self: datetime.today(), readonly=True, tracking=1)
     payment_method = fields.Selection(
         [
             ("cash","Cash"),
             ("transfer","Transfer")
-        ], string='Payment')
-    departure = fields.Datetime(string="Departure")
-    arrival = fields.Datetime(string="Arrival")
-    bus_id = fields.Many2one(comodel_name='res.bus', string="Bus")
+        ], string='Payment', tracking=1)
+    departure = fields.Datetime(string="Departure", tracking=1)
+    arrival = fields.Datetime(string="Arrival", tracking=1)
+    bus_id = fields.Many2one(comodel_name='res.bus', string="Bus", tracking=1)
     route_id = fields.Many2one(comodel_name='bus.route', string="Bus Route")
     baggage_ids = fields.One2many(comodel_name='baggage.baggage', inverse_name='schedule_id', string='Baggage')
     passenger_ids = fields.Many2many('res.passenger', string='Passenger')
     capacity = fields.Integer(string="Capacity", related="bus_id.capacity")
-    driver_id = fields.Many2one(comodel_name='hr.employee', string='Driver ', domain=[("is_driver", "=", True)])
+    driver_id = fields.Many2one(comodel_name='hr.employee', string='Driver ', domain=[("is_driver", "=", True)], tracking=1)
     driver_name = fields.Char(related='driver_id.name', string='Driver Name')
     driver_license = fields.Char(related='driver_id.driver_license', string='Driver License')
     driver_license_expired_date = fields.Date(related='driver_id.driver_license_expired_date')
